bfea799115f902d90ac7f3236bdf818841aea9d8
Daijiro Tsushima <daijiro.tsushima@gmail.com>
2023-11-15 14:28:23 -0500

    updates

--- SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -4,20 +4,16 @@ struct Chapter4View: View {
     var body: some View {
         VStack {
             Text("Chapter 4")
-                .font(.title)
+                .font(.largeTitle)
                 .padding()
-            
-            Image(systemName: "4.circle.fill")
+
+            Image(systemName: "book.circle.fill")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .scaledToFit()
                 .frame(width: 100, height: 100)
                 .padding()
-            
-            Text("Finally, Sir Arthur reached the hidden treasure, bringing peace and prosperity to the kingdom.")
-                .padding()
-            
-            Text("The end.")
-                .font(.title2)
+
+            Text("The end of the story.")
                 .padding()
         }
     }
